883b63ee4b226003b8f1d9c1816d9590b76991cc
Egaokira <egaokira@gmail.com>
2024-08-27 19:38:21 +0100

    starting models

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    PROPERTY_TYPES = (
+        ('Apartment', 'Apartment'),
+        ('House', 'House'),
+    )
+
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    property_type = models.CharField(max_length=50, choices=PROPERTY_TYPES)
+    number_of_units = models.IntegerField(default=1)
+    rental_cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+
--- property_management/settings.py
+++ property_management/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps 
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'properties',
+
 ]
 
 MIDDLEWARE = [
@@ -98,6 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 
 # Internationalization
